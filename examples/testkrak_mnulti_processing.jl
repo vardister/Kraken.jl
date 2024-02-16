d905311a1864aedf25c57efefe83b9a2e5656e19
Ariel Vardi <vardister@pm.me>
2024-02-16 15:23:50 -0500

    An example script that demonstrates how to run KRAKEN using multi processing

---
+++ examples/testkrak_mnulti_processing.jl
@@ -0,0 +1,48 @@
+"""
+Same as testkrak1.jl but with multi-threading enabled. This is a test to see if the multi-threading is working correctly with Kraken and if it is faster than the single-threaded version. Also, how to use it with `ccall`?
+"""
+
+using Distributed
+using SharedArrays
+addprocs(4)
+@everywhere using KRAKEN
+using GLMakie
+using BenchmarkTools
+
+@everywhere function main(freq)
+
+nm = 41
+hw = 71.0
+
+ssp, b, sspHS = env_builder(hw=hw)
+
+nsr = 1
+zsr = 18.0
+nrc = 70
+zrc = [0.0 hw]
+
+env_temp = Env(
+    ssp = ssp,
+    bottom = b,
+    sspHS = sspHS,
+    n_receivers = nrc,
+    z_receivers = zrc,
+    z_sources = zsr,
+    n_sources = nsr,
+)
+println(myid())
+
+return kraken(env_temp, freq; n_modes=nm)
+end
+
+freqs = fill(500.0, 100)
+rest = Vector{Any}(undef, length(freqs))
+res = Vector{Any}(undef, length(freqs))
+
+@benchmark for (i, freq) in enumerate(freqs)
+    rest[i] = main(freq)
+end
+
+@benchmark res .= pmap($main, $freqs)
+
+# We can see an increase in speed! And a good one as well!
