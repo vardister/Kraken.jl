bb899dea2c13b5c398b1540b8b2455cfa7e1da80
Ariel Vardi <vardister@pm.me>
2025-01-08 12:40:48 -0500

    First KRAKEN.jl set of tests.
    The 1st and only one is a test for the wavenumber estimation for the analytical Pekeris case. Hopefully more will come.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,17 @@
 using KRAKEN
 using Test
 
-@testset "KRAKEN.jl" begin
-    # Write your tests here.
+
+@testset "kraken_pekeris.jl" begin
+    env = PekerisUnderwaterEnv(1500.0, 1600.0, 1000.0, 1500.0, 100.0)
+    p = [env.c1, env.cb, env.ρ1, env.ρb, env.depth]
+    krs = find_kr(env, 100.0, p)
+    krs = round.(krs, digits=6)
+    # test the first 5 values of krs for the known pekeris waveguide
+    @test all((krs .- [0.417908, 0.414964, 0.409971, 0.40286, 0.39383]) .< 1e4)
+end
+
+
+@testset "kraken_core.jl" begin
+    
 end
