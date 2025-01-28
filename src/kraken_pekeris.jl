6d99fdf05f4f5a135f82e5f6012b9a64c01aeda9
Ariel Vardi <vardister@proton.me>
2025-01-28 15:17:29 -0500

    formatting and more

--- src/kraken_pekeris.jl
+++ src/kraken_pekeris.jl
@@ -29,17 +29,17 @@ export PekerisUnderwaterEnv, find_kr, get_modal_function, pressure_f, find_spans
 	env = PekerisUnderwaterEnv(1500.0, 1600.0, 1000.0, 2000.0, 100.0)
 	```
 """
-struct PekerisUnderwaterEnv{T1<:Real, T2<:Real, T3<:Real, T4<:Real, T5<:Real}
-	c1::T1
-	cb::T2
-	ρ1::T3
-	ρb::T4
-	depth::T5
+struct PekerisUnderwaterEnv{T1<:Real,T2<:Real,T3<:Real,T4<:Real,T5<:Real}
+    c1::T1
+    cb::T2
+    ρ1::T3
+    ρb::T4
+    depth::T5
 
-	function PekerisUnderwaterEnv(c1::T1, cb::T2, ρ1::T3, ρb::T4, depth::T5) where {T1, T2, T3, T4, T5}
-		@assert cb > c1 "Bottom sound speed must be greater than water sound speed"
-		new{T1, T2, T3, T4, T5}(c1, cb, ρ1, ρb, depth)
-	end
+    function PekerisUnderwaterEnv(c1::T1, cb::T2, ρ1::T3, ρb::T4, depth::T5) where {T1,T2,T3,T4,T5}
+        @assert cb > c1 "Bottom sound speed must be greater than water sound speed"
+        return new{T1,T2,T3,T4,T5}(c1, cb, ρ1, ρb, depth)
+    end
 end
 
 """
@@ -57,23 +57,25 @@ end
     # Returns
     - `Vector{Real}`: The horizontal wavenumbers.
 """
-function find_kr(env::PekerisUnderwaterEnv, f; n_points = 2_000, method = NewtonRaphson())
-	ω = 2π * f
-	kr_min, kr_max = extrema([ω / env.c1, ω / env.cb])
-	p = SA[env.c1, env.cb, env.ρ1, env.ρb, env.depth]
-	func(kr, p) =
-		@. nm.tan(nm.sqrt((ω / env.c1)^2 - kr^2) * env.depth) + (env.ρb / env.ρ1) * nm.sqrt((ω / env.c1)^2 - kr^2) / nm.sqrt(kr^2 - (ω / env.cb)^2)
+function find_kr(env::PekerisUnderwaterEnv, f; n_points=2_000, method=NewtonRaphson())
+    ω = 2π * f
+    kr_min, kr_max = extrema([ω / env.c1, ω / env.cb])
+    p = SA[env.c1, env.cb, env.ρ1, env.ρb, env.depth]
+    function func(kr, p)
+        @. nm.tan(nm.sqrt((ω / env.c1)^2 - kr^2) * env.depth) +
+            (env.ρb / env.ρ1) * nm.sqrt((ω / env.c1)^2 - kr^2) / nm.sqrt(kr^2 - (ω / env.cb)^2)
+    end
 
-	kr_try = range(kr_min + eps(kr_min), kr_max - eps(kr_min); length = n_points)
-	kr0 = find_spans(func, kr_try, p) |> collect
-	# kr0 = [0.39380573744004627, 0.4028554311353545, 0.40996684021864305, 0.4149566134428289, 0.41790332991380486]
-	# @show kr0
-	if isempty(kr0)
-		return Vector{eltype(kr_min)}()
-	end
-	prob = NonlinearProblem{false}(func, kr0, p)
-	sol = solve(prob, method)
-	return reverse(sol.u)
+    kr_try = range(kr_min + eps(kr_min), kr_max - eps(kr_min); length=n_points)
+    kr0 = collect(find_spans(func, kr_try, p))
+    # kr0 = [0.39380573744004627, 0.4028554311353545, 0.40996684021864305, 0.4149566134428289, 0.41790332991380486]
+    # @show kr0
+    if isempty(kr0)
+        return Vector{eltype(kr_min)}()
+    end
+    prob = NonlinearProblem{false}(func, kr0, p)
+    sol = solve(prob, method)
+    return reverse(sol.u)
 end
 
 """
@@ -111,11 +113,11 @@ end
     # Returns
     - `Real`: The solution.
 """
-function solve_pekeris_equation(span, func_to_min, p; method = ITP())
-	kr0 = mean(span)
-	prob = NonlinearProblem(func_to_min, kr0, p)
-	sol = solve(prob, TrustRegion())
-	return sol.u
+function solve_pekeris_equation(span, func_to_min, p; method=ITP())
+    kr0 = mean(span)
+    prob = NonlinearProblem(func_to_min, kr0, p)
+    sol = solve(prob, TrustRegion())
+    return sol.u
 end
 
 """
@@ -135,24 +137,24 @@ end
     - `Vector{Real}`: The modal function at the source depth.
 """
 function get_modal_function(env, krs, freq, zr, zs)
-	@unpack c1, cb, ρ1, ρb, depth = env
-	ω = 2π * freq
-	kzw = sqrt.((ω / c1)^2 .- krs .^ 2) # vertical wavenumber in the water column
-	kzb = sqrt.(krs .^ 2 .- (ω / cb)^2) # vertical wavenumber in the bottom half-space
-	# The amplitude `amplitude` of the modes
-	amplitude =
-		sqrt.(
-			(4 .* kzb .* kzw .* ρ1 .* ρb) ./
-			(2 .* kzw .* ρ1 .* sin.(depth .* kzw) .^ 2 .- kzb .* ρb .* (-2 .* depth .* kzw .+ sin.(2 .* depth .* kzw)))
-		)
-	# The mode function
-	function Ψ(z, mode)
-		amplitude[mode] * sin(kzw[mode] * z) * (z .<= depth) +
-		amplitude[mode] * sin(kzw[mode] * depth) * exp(-kzb[mode] * (z - depth)) * (z .> depth)
-	end
-	modes_zr = [Ψ(zr, mode) for mode in eachindex(krs)]
-	modes_zs = [Ψ(zs, mode) for mode in eachindex(krs)]
-	return modes_zr, modes_zs
+    @unpack c1, cb, ρ1, ρb, depth = env
+    ω = 2π * freq
+    kzw = sqrt.((ω / c1)^2 .- krs .^ 2) # vertical wavenumber in the water column
+    kzb = sqrt.(krs .^ 2 .- (ω / cb)^2) # vertical wavenumber in the bottom half-space
+    # The amplitude `amplitude` of the modes
+    amplitude =
+        sqrt.(
+            (4 .* kzb .* kzw .* ρ1 .* ρb) ./
+            (2 .* kzw .* ρ1 .* sin.(depth .* kzw) .^ 2 .- kzb .* ρb .* (-2 .* depth .* kzw .+ sin.(2 .* depth .* kzw)))
+        )
+    # The mode function
+    function Ψ(z, mode)
+        return amplitude[mode] * sin(kzw[mode] * z) * (z .<= depth) +
+               amplitude[mode] * sin(kzw[mode] * depth) * exp(-kzb[mode] * (z - depth)) * (z .> depth)
+    end
+    modes_zr = [Ψ(zr, mode) for mode in eachindex(krs)]
+    modes_zs = [Ψ(zs, mode) for mode in eachindex(krs)]
+    return modes_zr, modes_zs
 end
 
 """
@@ -173,19 +175,19 @@ end
     # Returns
     - `Complex{Real}`: The pressure field.
 """
-function pressure_f(env::PekerisUnderwaterEnv, krs, freq, r, zs, zr; t0 = 0.1, max_modes = Inf)
-	if freq == 0
-		return 0.0 + 0.0im
-	end
-	if length(krs) == 0
-		return 0.0 + 0.0im
-	end
-	@unpack c1, cb, ρ1, ρb, depth = env
-	ψ_zr, ψ_zs = get_modal_function(env, krs, freq, zr, zs)
-	t_offset = r / min(c1, cb) - t0  # align window correctly in time
-	n_modes = min(max_modes, length(krs)) |> Int
-	pf =
-		-1im * exp(-1im * π / 4) / (ρ1 * sqrt(8π * r)) *
-		sum([ψ_zs[mode] * ψ_zr[mode] * exp(-1im * krs[mode] * r) / sqrt(krs[mode]) for mode in range(1, n_modes)])
-	return pf * exp(2im * pi * freq * t_offset)
+function pressure_f(env::PekerisUnderwaterEnv, krs, freq, r, zs, zr; t0=0.1, max_modes=Inf)
+    if freq == 0
+        return 0.0 + 0.0im
+    end
+    if length(krs) == 0
+        return 0.0 + 0.0im
+    end
+    @unpack c1, cb, ρ1, ρb, depth = env
+    ψ_zr, ψ_zs = get_modal_function(env, krs, freq, zr, zs)
+    t_offset = r / min(c1, cb) - t0  # align window correctly in time
+    n_modes = Int(min(max_modes, length(krs)))
+    pf =
+        -1im * exp(-1im * π / 4) / (ρ1 * sqrt(8π * r)) *
+        sum([ψ_zs[mode] * ψ_zr[mode] * exp(-1im * krs[mode] * r) / sqrt(krs[mode]) for mode in range(1, n_modes)])
+    return pf * exp(2im * pi * freq * t_offset)
 end
