49d9343c1e0449b6348609c84e61f1b6943fb468
Ariel Vardi <vardister@pm.me>
2025-12-11 11:36:20 -0500

    Moved FORTRAN codes to new package KrakenFortran.jl

--- src/Kraken.jl
+++ src/Kraken.jl
@@ -6,15 +6,4 @@ include("kraken_basic.jl")
 include("kraken_pekeris.jl")
 
 include("kraken_standard_environments.jl")
-
-### Legacy Fortran implementation
-include("kraken_fortran.jl")
-export kraken
-export EnvKRAKEN
-export env_builder
-export pf_adiabatic
-export pf_adiabatic_signal
-export pf_signal
-export pressure_field_fortran
-
 end
